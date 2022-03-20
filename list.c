89f39df0e26de62cb0322d4b63a221a3d3c7eb95
Your Name <you@example.com>
2022-03-19 22:45:39 -0300

    new try

--- list.c
+++ list.c
@@ -46,11 +46,9 @@ void * firstList(List * list) {
 }
 
 void * nextList(List * list) {
-  if(list->current->next != NULL){
-    printf("\nnodo: %d\n",list->current->data);
+  if(list->current->next != NULL && list->current != NULL){
     list->current=list->current->next;
-    printf("\nnodo: %d\n",list->current->data);
-    return NULL;
+    return list->current->data;
   }
   return NULL;
 }
